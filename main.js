7117ea68f3c6b0e913a242e26ec42a6e501112a1
saharrshirazii <saharr.shirazii@gmail.com>
2025-09-24 10:42:45 +0200

    Remove string spaces

---
+++ main.js
@@ -0,0 +1,11 @@
+
+//Remove string spaces
+//https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+//Goal: Remove spaces from a string and return a clean string.
+//Inputs: A string with spaces.
+//Return: return a string without spaces.
+//Step1: create a function with parameter string .
+//Step2: find all spaces inside the string.
+//Step3: Remove all spaces inside string.
+//Step4: Return final string without spaces.
